f8dd77d053910e61a529e516f16b205953829610
Isaiah <isaiah_manzo@outlook.com>
2025-05-05 18:58:40 +0200

    info,levels,settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
+import { InfoComponent } from './info/info.component';
+import { LevelsComponent } from './levels/levels.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet,SettingsComponent,InfoComponent,LevelsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
